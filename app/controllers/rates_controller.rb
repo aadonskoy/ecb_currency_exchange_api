ccab3115e43a6c4c2945b8552a2c01386b6ad8b7
Alexey Donskoy <aadonskoy@gmail.com>
2016-11-05 13:33:11 +0200

    Adds endpoint for conversion.

---
+++ app/controllers/rates_controller.rb
@@ -0,0 +1,14 @@
+class RatesController < ApplicationController
+  def show
+    daily_rate = DailyRate.nearest_to_date(parsed_date).last!
+    unless performed?
+      render json: { result: daily_rate.usd_to_euro(params[:amount].to_f) }, status: :ok
+    end
+  end
+
+  def parsed_date
+    params[:id].to_date
+  rescue => error
+    render json: { error: 'Invalid date' }, status: :unprocessable_entity
+  end
+end
--- app/models/daily_rate.rb
+++ app/models/daily_rate.rb
@@ -1,4 +1,12 @@
 class DailyRate < ApplicationRecord
   validates :date, :rate, presence: true
   validates :date, uniqueness: true
+
+  def self.nearest_to_date(selected_date)
+    where('date <= ?', selected_date).order(:date)
+  end
+
+  def usd_to_euro(usd)
+    (usd * rate * 100).ceil / 100.0
+  end
 end
