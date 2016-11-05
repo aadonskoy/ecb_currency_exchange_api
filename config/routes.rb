d86f63df69e16f6c5f348c2342ccfca6e2d32e3b
Alexey Donskoy <aadonskoy@gmail.com>
2016-11-05 13:38:33 +0200

    Adds api versioning. Adds default format.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  resources :rates, only: :show
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :rates, only: :show
+    end
+  end
 end
